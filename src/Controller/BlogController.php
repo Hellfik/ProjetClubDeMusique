302ea59f627a599801acaa3d93b9aa18f2b9dbea
Mika Fayeulle <mika.fayeulle@gmail.com>
2019-08-06 16:57:19 +0200

    Add Article Entity + fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
+     *
      */
-    public function blog()
+    public function blog(ArticleRepository $repo)
     {
-        return $this->render('blog/blog.html.twig');
+        $articles = $repo->findById(); 
+        return $this->render('blog/blog.html.twig', [
+            'articles' => $articles
+        ]);
     }
+    /**
+     * @param Article $article
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Article $article){
+         return $this->render('blog/show.html.twig', [
+             'article' => $article
+         ]);
+    }
+
 }
