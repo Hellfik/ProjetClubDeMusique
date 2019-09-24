bcfd8a44fadeb48a05b67c40bce508010b82fcce
Stephane <lecoutrestephane@outlook.com>
2019-09-24 11:24:06 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,26 +2,22 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('email')
-            ->add('message', TextareaType::class)
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            // Configure your form options here
         ]);
     }
 }
